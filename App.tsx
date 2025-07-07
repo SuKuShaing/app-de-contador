d94a57cf20a4fba747dd0e49bafa3aeb185868a6
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-07-07 17:23:16 -0400

    componente reutilizable, botón flotante

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import FAB from "./components/FAB";
 
 export default function App() {
 	const [count, setCount] = useState(10);
@@ -11,25 +12,19 @@ export default function App() {
 			<Text style={styles.textNormal}>Holis bb</Text>
 			<Text style={styles.textHuge}>{count}</Text>
 
-			<Pressable
-				style={[styles.floatingButton, styles.floatingButtonRight]}
-				onPress={() => {setCount(count + 1)}}
-				>
-				<Text style={{color: 'white', fontSize: 20}}>+1</Text>
-			</Pressable>
+			<FAB
+				label="+1"
+				onPress={() => setCount(count + 1)}
+				onLongPress={() => setCount(0)}
+				position="right"
+			/>
 
-			<Pressable
-				style={[styles.floatingButton, styles.floatingButtonLeft]}
-				onPress={() => {setCount(count - 1)}}
-				onLongPress={() => {setCount(0)}}
-			>
-				<Text style={{color: 'white', fontSize: 20}}>-1</Text>
-			</Pressable>
-
-			{/* <TouchableOpacity>
-				{/* similar al Pressable pero ya tiene incorporado que pierda opacidad al momento de presionarlo }
-				<Text>-1</Text>
-			</TouchableOpacity> */}
+			<FAB
+				label="-1"
+				onPress={() => setCount(count - 1)}
+				onLongPress={() => setCount(0)}
+				position="left"
+			/>
 
 			<StatusBar style="auto" />
 		</View>
@@ -52,30 +47,5 @@ const styles = StyleSheet.create({
 	textHuge: {
 		fontSize: 120,
 		fontWeight: "100",
-	},
-
-	floatingButton: {
-		backgroundColor: "#65558F",
-		position: "absolute",
-		padding: 20,
-		borderRadius: 15,
-		shadowColor: "#000", // solo para IOS
-		shadowOffset: {
-			width: 0,
-			height: 4,
-		},
-		shadowOpacity: 0.3,
-		shadowRadius: 4.65,
-		elevation: 3, // solo para Android
-	},
-
-	floatingButtonRight: {
-		bottom: 30,
-		right: 30,
-	},
-
-	floatingButtonLeft: {
-		bottom: 30,
-		left: 30,
 	},
 });
---
+++ components/FAB.tsx
@@ -0,0 +1,71 @@
+// FAB: Floating Action Button
+
+import { Text, Pressable, StyleSheet } from "react-native";
+
+interface Props {
+	label: string;
+	position?: "left" | "right";
+
+	// Methods
+	onPress?: () => void;
+	onLongPress?: () => void;
+}
+
+export default function FAB({
+	label,
+	onPress,
+	onLongPress,
+	position = "right",
+}: Props) {
+	return (
+		<>
+			<Pressable
+				style={[styles.floatingButton, position === "right" ? styles.floatingButtonRight : styles.floatingButtonLeft]}
+				onPress={onPress}
+				onLongPress={onLongPress}
+			>
+				<Text style={{ color: "white", fontSize: 20 }}>{label}</Text>
+			</Pressable>
+
+			{/*
+            <Pressable
+				style={[styles.floatingButton, styles.floatingButtonLeft]}
+				onPress={() => {setCount(count - 1)}}
+				onLongPress={() => {setCount(0)}}
+                >
+				<Text style={{color: 'white', fontSize: 20}}>-1</Text>
+			</Pressable>
+            
+			<TouchableOpacity>
+            {/* similar al Pressable pero ya tiene incorporado que pierda opacidad al momento de presionarlo }
+            <Text>-1</Text>
+			</TouchableOpacity> */}
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	floatingButton: {
+		backgroundColor: "#65558F",
+		position: "absolute",
+		bottom: 30,
+		padding: 20,
+		borderRadius: 15,
+		shadowColor: "#000", // solo para IOS
+		shadowOffset: {
+			width: 0,
+			height: 4,
+		},
+		shadowOpacity: 0.3,
+		shadowRadius: 4.65,
+		elevation: 3, // solo para Android
+	},
+
+	floatingButtonRight: {
+		right: 30,
+	},
+
+	floatingButtonLeft: {
+		left: 30,
+	},
+});
